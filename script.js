9269dcd74abb0f113d4193101169ce2d5d313993
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-02-19 13:07:43 -0500

    Constructors and the new Operator

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// Contructor functions start with a capital letter
+// Arrow functions down work in constructor functions because they have no this keyword
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never create a function in a constructor function
+  this.calcAge = function () {
+    console.log(2037 - birthYear);
+  };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically returns {}
