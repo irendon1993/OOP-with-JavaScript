ac9a7be47e057876cc66e8ec08f280547a1f56ef
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-02-19 19:13:17 -0500

    ES6 Classes

--- script.js
+++ script.js
@@ -2,30 +2,49 @@
 
 // Contructor functions start with a capital letter
 // Arrow functions down work in constructor functions because they have no this keyword
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  // Never create a function in a constructor function
-  //   this.calcAge = function () {
-  //     console.log(2037 - birthYear);
-  //   };
-};
+//   // Never create a function in a constructor function
+//   //   this.calcAge = function () {
+//   //     console.log(2037 - birthYear);
+//   //   };
+// };
 
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+// const jonas = new Person('Jonas', 1991);
+// console.log(jonas);
 
 // 1. New {} is created
 // 2. function is called, this = {}
 // 3. {} linked to prototype
 // 4. function automatically returns {}
 
-// Prototypes
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
+// // Prototypes
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
 
-jonas.calcAge();
+// jonas.calcAge();
 
-console.log(jonas.__proto__);
-console.log(jonas.__proto__.__proto__);
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__.__proto__);
+
+// Class Expression
+// const PersonCl = class {}
+
+// Class Declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
